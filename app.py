0dd4a31fbe9a620f5ac6d4e0f7ada65576d135a7
Liron <Liron.sindury@gmail.com>
2026-05-20 00:33:59 +0300

    Fix duplicate db.create_all

--- app.py
+++ app.py
@@ -217,6 +217,5 @@ def create_worker():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
         logger.info("Database ready.")
     app.run(host="0.0.0.0", port=5000, debug=True)
